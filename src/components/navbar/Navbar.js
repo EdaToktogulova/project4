08e82dcda485019933de251bd619b8f6da5a7163
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-05-10 22:47:58 +0600

    Add new component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,23 +1,27 @@
-import React from "react";
-import {FaIceCream, FaBars} from "react-icons/fa";
+import React, {useState} from "react";
+import {FaIceCream, FaBars, FaTimes} from "react-icons/fa";
 import './Navbar.css'
 
 const Navbar = () => {
+   const [nav, setNav] = useState(false)
+   const handleNav = () => setNav(!nav)
+
    return (
       <div className="navbar">
-         <div className="container"></div>
-         <FaIceCream  />
-            <ul className="nav-menu">
-               <li>Home</li>
-               <li>Order</li>
-               <li>Menu</li>
-               <li>Delivery</li>
-               <li>Contact</li>
-            </ul>
-            <div className="iceCream">
-               <FaBars />
-            </div>
+      <div className="container">
+         <div><FaIceCream size={40} style={{marginleft: '4px'}} /></div>
+         <ul className={nav ? 'nav-menu active' : 'nav-menu'}>
+            <li>Home</li>
+            <li>Order</li>
+            <li>Menu</li>
+            <li>Delivery</li>
+            <li>Contact</li>
+         </ul>
+      <div className="IceCream" onClick={handleNav}>
+         {nav ? (<FaTimes size={20} style={{color: '#ffffff'}} />) : (<FaBars size={20} />)}
       </div>
+      </div>
+   </div>
    );
 }
 
