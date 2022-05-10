6848980b6dacf058a4d3567902eabb29999940d3
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-05-10 22:47:45 +0600

    Add new file jsx

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './Header.css';
+
+const Header = () => {
+   return (
+      <div className="header">
+         <div className="container">
+            <div className="content">
+               <div className="text">
+                  <p>welcome to our</p>
+               </div>
+               <h1>Ice-Cream</h1>
+               <h1>Shop</h1>
+               <button className="btn">Delivery</button>
+               <button className="btn">Take-out</button>
+            </div>
+         </div>
+      </div>
+   );
+}
+
+export default Header;
