e16bb889ccd916b0a54e877cf960f2853fec6eec
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-05-11 09:44:35 +0600

    Add new component

---
+++ src/components/food/Food.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Food.css';
+
+const Food = () => {
+   return (
+      <div className="food">
+         <div className="container">
+            <div className="left"></div>
+            <div className="right"></div>
+         </div>
+      </div>
+   )
+}
+
+export default Food;
