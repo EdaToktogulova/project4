1d205afa836b792cf914489712a6df32db678dea
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-05-11 11:32:02 +0600

    News

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/food/Food";
+import Food from "./components/food/Food";
+import Footer from "./components/footer/Footer";
 
 // Import images
-import 
+import Craft1 from './assest/craft1.jpg'
+import Craft2 from './assest/craft2.jpg'
+import Violet1 from './assest/violet1.jpg'
+import Violet2 from './assest/violet2.jpg'
+import Footer1 from './assest/footer1.jpg'
+
 
 function App() {
   return (
     <>
     <Navbar />
     <Header />
+    <Food bgImg1={Craft1} bgImg2={Craft2} />
+    <Food bgImg={Violet1} />
+    <Food bgImg={Violet2} />
     <Footer />
     </>
   );
