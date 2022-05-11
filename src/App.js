015396aac1b1918bf4932c684b898f7deb1569d9
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-05-11 09:43:58 +0600

    import to Food

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/food/Food";
+
+// Import images
+import 
 
 function App() {
   return (
     <>
     <Navbar />
     <Header />
+    <Footer />
     </>
   );
 }
